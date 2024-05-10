1ca5bfdfe049c78c5628005d730fb4634ba2c5b7
Laura <46911587+laura2233@users.noreply.github.com>
2024-05-10 12:47:59 -0400

    Create meow.js

---
+++ meow.js
@@ -0,0 +1,16 @@
+function initEmbeddedMessaging() {
+		try {
+			embeddedservice_bootstrap.settings.language = 'en_US'; // For example, enter 'en' or 'en-US'
+
+			embeddedservice_bootstrap.init(
+				'00D6t000001Qvai',
+				'DP_Chat',
+				'https://softerware--full.sandbox.my.site.com/ESWDPChat1655493128387',
+				{
+					scrt2URL: 'https://softerware--full.sandbox.my.salesforce-scrt.com'
+				}
+			);
+		} catch (err) {
+			console.error('Error loading Embedded Messaging: ', err);
+		}
+};
